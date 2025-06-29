09b8f5b937d7bafd049c25187893ecd1c73324ae
Igor Tyshchenko <33253240+igoresso@users.noreply.github.com>
2025-06-29 16:04:03 +1000

    Deploy to GH pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
